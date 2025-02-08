1cafe1a8f61dfae8c97266afce4a659c31457a13
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-08 19:10:01 +0530

    feat: Add Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-red-500 underline">
+        Hello world!
+      </h1>
+      <Button>Hello</Button>
+    </div>
   );
 }
 
