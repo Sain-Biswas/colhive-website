590e4745f5c1dcf71e19c0fbb3d322f9e350e43e
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-11 13:08:19 +0530

    Add new fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,109 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
 import { Toaster } from "@/components/ui/sonner";
 
 import { ThemeProvider } from "@/components/providers/theme-providers";
 
+import { cn } from "@/lib/utils";
 import { TRPCReactProvider } from "@/trpc/trpc-react-provider";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const gilroySansSerif = localFont({
+  src: [
+    {
+      path: "../fonts/Gilroy-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-UltraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Gilroy-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-UltraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-RegularItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Gilroy-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-gilroy-sans-serif",
 });
 
 export const metadata: Metadata = {
@@ -31,7 +118,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={cn(
+          "font-gilroy min-h-screen antialiased",
+          gilroySansSerif.variable
+        )}
+      >
         <ThemeProvider>
           <TRPCReactProvider>
             {children}
