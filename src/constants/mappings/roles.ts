bb4ef9578f729e7cd0ee1a5bd881edb7167091d4
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-24 14:40:05 +0530

    feat(members): Add filtering in members table

---
+++ src/constants/mappings/roles.ts
@@ -0,0 +1,19 @@
+import { Building2Icon, ShieldIcon, UserRoundIcon } from "lucide-react";
+
+export const roles = [
+  {
+    label: "Owner",
+    value: "owner",
+    icon: Building2Icon,
+  },
+  {
+    label: "Admin",
+    value: "admin",
+    icon: ShieldIcon,
+  },
+  {
+    label: "Member",
+    value: "member",
+    icon: UserRoundIcon,
+  },
+];
