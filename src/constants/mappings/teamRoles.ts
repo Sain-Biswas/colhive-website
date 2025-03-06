d01fd1865d90aaa35e42ebb464187d2e5e27eabb
Sain-Biswas <sain.biswas614@gmail.com>
2025-03-06 23:40:23 +0530

    feat(projects): Add functionality to create new projects

---
+++ src/constants/mappings/teamRoles.ts
@@ -0,0 +1,30 @@
+export const teamRoles = [
+  {
+    value: "manager",
+    lable: "Manager",
+  },
+  {
+    value: "team-lead",
+    lable: "Team Lead",
+  },
+  {
+    value: "designer",
+    lable: "Designer",
+  },
+  {
+    value: "developer",
+    lable: "Developer",
+  },
+  {
+    value: "tester",
+    lable: "Tester",
+  },
+  {
+    value: "member",
+    lable: "Member",
+  },
+  {
+    value: "sub-lead",
+    lable: "Sub Lead",
+  },
+];
