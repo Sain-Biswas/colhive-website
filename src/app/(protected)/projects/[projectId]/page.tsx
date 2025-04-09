281da33c1a7c9d6e8aa168abbbb4cf416a7a6009
Sain-Biswas <sain.biswas614@gmail.com>
2025-04-09 14:45:56 +0530

    fix: Sinple Project page props Promise error

--- src/app/(protected)/projects/[projectId]/page.tsx
+++ src/app/(protected)/projects/[projectId]/page.tsx
@@ -1,9 +1,14 @@
-interface SingleProjectPageProps {
-  params: {
+import { PageProps } from "../../../../../.next/types/app/page";
+
+interface SingleProjectPageProps extends PageProps {
+  params: Promise<{
     projectId: string;
-  };
+  }>;
 }
 
-export default function SingleProjectPage({ params }: SingleProjectPageProps) {
-  return <div>Hello, {params.projectId}</div>;
+export default async function SingleProjectPage({
+  params,
+}: SingleProjectPageProps) {
+  const { projectId } = await params;
+  return <div>Hello, {projectId}</div>;
 }
