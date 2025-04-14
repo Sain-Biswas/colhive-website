27a8dc247945e3c0b113bd6048e9bfc8a8bbfea3
Sain-Biswas <sain.biswas614@gmail.com>
2025-04-14 14:20:23 +0530

    feat: add footer section of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ import {
   ArrowRightIcon,
   BellIcon,
   CheckCircleIcon,
+  Facebook,
+  Instagram,
   LayoutIcon,
+  Linkedin,
+  Twitter,
   UsersIcon,
 } from "lucide-react";
 
@@ -86,6 +90,23 @@ const testimonials = [
     },
   },
 ];
+
+const navigation = {
+  main: [
+    { name: "About", href: "#" },
+    { name: "Blog", href: "#" },
+    { name: "Jobs", href: "#" },
+    { name: "Press", href: "#" },
+    { name: "Accessibility", href: "#" },
+    { name: "Partners", href: "#" },
+  ],
+  social: [
+    { name: "Facebook", href: "#", icon: Facebook },
+    { name: "Instagram", href: "#", icon: Instagram },
+    { name: "Twitter", href: "#", icon: Twitter },
+    { name: "LinkedIn", href: "#", icon: Linkedin },
+  ],
+};
 
 export default function Home() {
   return (
@@ -250,7 +271,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-
       <section className="bg-background py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-xl text-center">
@@ -286,6 +306,41 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <footer className="bg-secondary">
+        <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+          <nav
+            className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+            aria-label="Footer"
+          >
+            {navigation.main.map((item) => (
+              <div key={item.name} className="pb-6">
+                <a
+                  href={item.href}
+                  className="text-secondary-foreground hover:text-primary text-sm leading-6"
+                >
+                  {item.name}
+                </a>
+              </div>
+            ))}
+          </nav>
+          <div className="mt-10 flex justify-center space-x-10">
+            {navigation.social.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-secondary-foreground hover:text-primary"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+            &copy; 2025 ColHive, Inc. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </main>
   );
 }
